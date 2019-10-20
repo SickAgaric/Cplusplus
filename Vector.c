3eda2dd6e1a2761637f6c1dc41e6bc9d1dfa76a8
mochu823@yeah.net <mochu823@yeah.net>
2019-10-20 20:02:06 +0800

    提交代码

--- Vector.c
+++ Vector.c
@@ -66,7 +66,111 @@ namespace cx
 			return _start[pos];
 		}
 
-		
+		void resize(size_t n, const T& v = T())
+		{
+			if (n < size())
+			{
+				_finish = _start + n;
+				return;
+			}
+
+			if (n>size())
+			{
+				reserve(n);
+			}
+
+			while (_finish != _start + n)
+			{
+				*_finish = v;
+				_finish++;
+			}
+		}
+
+		void reserve(size_t n)
+		{
+			if (n > capacity())
+			{
+				size_t sz = size();  //保存n是因为，当_start改变后，size()也会改变，会导致_finish出错
+				T* a = new T[n];
+				if (_start)
+				{
+					memcpy(a, _start, sizeof(T)*n);
+
+					delete[] _start;
+				}
+
+				_start = a;
+				_finish = _start + sz;
+				//_finsh = _start + size();
+				_end_of_storage = _start + n;
+
+				/*size_t sz = size();
+				T* s = new[n];
+				if (_start)
+				{
+				memcpy(s, _start, sizeof(T)*n);
+
+				delete[] _start;
+				}
+				_start = s;
+				_finish = _start + sz;
+				_end_of_storage = _start + n;*/
+			}
+		}
+
+		void push_back(const T& x)
+		{
+			if (_start == _end_of_storage)
+			{
+				size_t newcapacity = capacity() == 0 ? 2 : capacity() * 2;
+				reserve(newcapacity);
+			}
+
+			*_finish = x;
+			_finish++;
+		}
+
+		void pop_back()
+		{
+			assert(_start < _finish);
+			--_finish;
+		}
+
+		void insert(iterator pos, const T& x)
+		{
+			if (_finish == _end_of_storage)
+			{
+				size_t sz = pos - _start;
+				size_t newcapacity = capacity() == 0 ? 2 : capacity() * 2;
+				reserve(newcapacity);
+				//reserve(size() * 2);
+				pos = sz + _start;
+			}
+
+			iterator end = _finish - 1;
+
+			while (end != pos)
+			{
+				*(end + 1) = *end;
+				end--;
+			}
+
+			*pos = x;
+			_finish++;
+		}
+
+		void erase(iterator pos)
+		{
+			assert(pos < _finish&&pos >= _start);
+			iterator index = pos + 1;
+			while (index != _finish)
+			{
+				*index = *(index + 1);
+				index++;
+			}
+
+			_finish--;
+		}
 
 
 	private:
