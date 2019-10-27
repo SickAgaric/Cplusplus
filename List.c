bd93a09e7598a06f11066e03a6468df462a0e13a
mochu823@yeah.net <mochu823@yeah.net>
2019-10-27 20:13:40 +0800

    修改代码

--- List.c
+++ List.c
@@ -1,239 +1,207 @@
 #include<iostream>
 
+
 using namespace std;
+
 
 namespace cx
 {
 	template<class T>
 
-	struct _list_node
+	struct __list_node
 	{
-
-		_list_node<T>* _next;
-		_list_node<T>* _prev;
-		T _date;
-
-		_list_node(const T& x = T())
-			:_date(x)
+		__list_node<T>* _next;
+		__list_node<T>* _prev;
+		T _data;
+		
+		__list_node(const T& x = T())
+			:_data(x)
 			, _next(nullptr)
 			, _prev(nullptr)
 		{}
-
 	};
 
-	template<class T,class ref,class ptr>
+	template<class T,class Ref,class Ptr>
 
-	class _list_iterator
+	struct __list_iterator
 	{
-		typedef _list_iterator<T> node;
-		typedef _list_iterator<T, ref, ptr> self;
+		typedef __list_node<T> node;
+		typedef __list_iterator<T, Ref, Ptr> Self;
 
 		node* _node;
 
-		__list_iterator(node _node)
+		__list_iteator(node* node)
 			:_node(node)
 		{}
 
-		ref operator*()
+		Ref operator*()//但会T&，内置类型，传引用
 		{
-			return _node->date;
+			return _node->_data;
 		}
 
-		ptr operator->()
+		Ptr operator->()//返回T*类型，用于方便自定义类型
 		{
-			return &_node->date;
+			return &_node->_data;
 		}
-
-		self operator++()
+		Self& operator++()
 		{
-			_node = _node->next;
+			_node = _node->_next;
 			return *this;
 		}
 
-		self operator++(int)
+		Self operator++(int)
 		{
-			_list_iterator<T> tmp(*this);
-			_node = _node->next;
+			__list_node<T> tmp(*this);
+			_node = _node->_next;
 			return tmp;
 		}
 
-		self operator--()
+		Self& operator--()
 		{
-			_node = _node->prev;
+			_node = _node->_prev;
 			return *this;
 		}
 
-		self operator--(int)
+		Self operator--(int)
 		{
-			_list_iterator<T> tmp(*this);
-			_node = _node->prev;
+			__list_iterator<T> tmp(*this);
+			_node = _node->_prev;
 			return tmp;
 		}
-
-		bool operator!=(const self& it)
-		{
-			return _node != it._node;
-		}
-
-		bool operator==(const self& it)
+		
+		bool operator==(const Self& it)
 		{
 			return _node == it._node;
 		}
 
+		bool operator!=(const Self& it)
+		{
+			return _node != it._node;
+		}
 	};
 
-	template<class T>
-
-	class List
+	template <class T>
+	class list
 	{
 		typedef __list_node<T> node;
+
 	public:
 		typedef __list_iterator<T, T&, T*> iterator;
 		typedef __list_iterator<T, const T&, const T*> const_iterator;
 
 		const_iterator begin() const
 		{
-			return const_iterator (_head->next);
+			return const_iterator(_head->_next);
 		}
 
 		const_iterator end() const
 		{
-			return const_iterator (_head->prev);
+			return const_iterator(_head);
 		}
 
 		iterator begin()
 		{
-			return iterator (_head->next);
+			return iterator(_head->next);
 		}
+
 
 		iterator end()
 		{
-			return iterator (_head->prev);
+			return iterator(_head);
 		}
 
-		List()
+		/*list(const list<T>& l)
+		{
+			_head = new node;
+			_head->_next = _head;
+			_head->_prev = _head;
+
+			auto it = l.beginl
+
+			while (it != l.end())
+			{
+				push_back(*it);
+				++it;
+			}
+		}*/
+
+		/*list<T>& operator=(list<T> l)
+		{
+
+		}*/
+
+		list()
 		{
 			_head = new node(T());
-			_head->next = _head;
-			_head->prev = _head;
+			_head->_next = _head;
+			_head->_prev = _head;
 		}
 
-		~List()
+		/*~list()
 		{
-			clear();
 
-			delete[] _head;
-			_head = nullptr;
 		}
 
 		void clear()
 		{
-			iterator it = begin();
 
-			while (it != end())
-			{
-				it = erase(it);
-			}
-		}
-
-
-
-		List(const List<T>& l)
-		{
-			_head = new node;
-			_head->next = _head;
-			_head->prev = _head;
-
-			auto it = l.begin();
-
-			while (it != l.end)
-			{
-				push_back(*it);
-				it++;
-			}
-
-		}
-
-		list<T>& operator=(list<T> l)
-		{
-			swap(_head, l._head);
-			return *this;
-		}
+		}*/
 
 		void push_back(const T& x)
 		{
-			insert(end(), x);
-		}
-
-		void push_front(const T& x)
-		{
-			insert(begin(), x);
-		}
-
-		void pop_back()
-		{
-			erase(--end());
-		}
-
-		void pop_front()
-		{
-			erase(begin());
-		}
-
-		void insert(iterator pos, const T& x)
-		{
-			node* cur = pos._node;
-			node* prev = cur->_prev;
+			node* tail = _head->_prev;
 			node* newnode = new node(x);
 
-			// prev newnode cur
-			prev->_next = newnode;
-			newnode->_prev = prev;
-			newnode->_next = cur;
-			cur->_prev = newnode;
+			tail->_next = newnode;
+			newnode->_prev = tail;
+			_head->_prev = newnode;
+			newnode->_next = _head;
+		}
+
+		/*void push_front(const T& x)
+		{
+
+		}*/
+
+		/*void pop_back()
+		{
+
+		}*/
+
+		/*void insert(iterator pos, const T& x)
+		{
+
 		}
 
 		iterator erase(iterator pos)
 		{
-			node* cur = pos._node;
-			assert(cur != _head);
 
-			node* prev = cur->_prev;
-			node* next = cur->_next;
+		}*/
 
-			prev->_next = next;
-			next->_prev = prev;
-
-			delete cur;
-
-			return iterator(next);
-		}
 	private:
 		node* _head;
 	};
-	void print_list(const List<int>& l)
+
+	/*void print_list(const list<int>& l)
 	{
-		List<int>::const_iterator it = l.begin();
-		while (it != l.end())
+
+	}*/
+
+	void test()
+	{
+		list<int> l;
+		l.push_back(1);
+		l.push_back(2);
+		l.push_back(3);
+		l.push_back(4);
+
+		list<int>::iterator it = l.begin;
+
+		while (it != l.end());
 		{
-			//*it = 10; // it.operator*() = 10;
 			cout << *it << " ";
 			++it;
 		}
 		cout << endl;
 	}
-}
-
-
-
-void Test()
-{
-	cx::List<int> l;
-	l.push_back(1);
-	l.push_back(2);
-	l.push_back(3);
-	l.push_back(4);
-	l.pop_back();
-	cx::print_list(l);
-
 }
